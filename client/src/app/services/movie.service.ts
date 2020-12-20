f5b2c66af53d626dfa3ad0bf0ff16e0411a43127
pivotrooky <towagner@gmail.com>
2020-12-20 15:20:57 -0300

    Excludes games from search results so that problems don't arise in the future

--- client/src/app/services/movie.service.ts
+++ client/src/app/services/movie.service.ts
@@ -1,29 +1,39 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
- 
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
+
 // Typescript custom enum for search types (optional)
 export enum SearchType {
-  all = '',
-  movie = 'movie',
-  series = 'series',
+  all = "",
+  movie = "movie",
+  series = "series",
 }
- 
-@Injectable({
-  providedIn: 'root'
-})
 
+@Injectable({
+  providedIn: "root",
+})
 export class MovieService {
-  url = 'http://www.omdbapi.com/';
+  url = "http://www.omdbapi.com/";
   apiKey = "20dac387";
 
-  constructor(private http: HttpClient) { }
-  
+  constructor(private http: HttpClient) {}
+
   searchData(title: string, type: SearchType): Observable<any> {
-    return this.http.get(`${this.url}?s=${encodeURI(title)}&type=${type}&apikey=${this.apiKey}`).pipe(
-      map(results => {console.log(results, "results"); return results['Search']})
-    );
+    return this.http
+      .get(
+        `${this.url}?s=${encodeURI(title)}&type=${type}&apikey=${this.apiKey}`
+      )
+      .pipe(
+        map((results) => {
+          console.log(results, "results");
+          const filteredResults = results["Search"].filter(
+            (item) => item.Type !== "game"
+          );
+          //I think it's best for the purposes of this app to only include movies/series
+          return filteredResults;
+        })
+      );
   }
   getDetails(id) {
     return this.http.get(`${this.url}?i=${id}&plot=full&apikey=${this.apiKey}`);
