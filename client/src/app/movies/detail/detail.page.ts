54a013c76a8ee71489070d15af5feb4e03e8e3bc
pivotrooky <towagner@gmail.com>
2020-12-20 15:21:58 -0300

    Adds basic card for when user clicks specific search result item.

--- client/src/app/movies/detail/detail.page.ts
+++ client/src/app/movies/detail/detail.page.ts
@@ -1,15 +1,27 @@
+import { MovieService } from './../../services/movie.service';
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute } from '@angular/router';
+ 
 @Component({
-  selector: 'app-detail',
+  selector: 'app-movie-details',
   templateUrl: './detail.page.html',
   styleUrls: ['./detail.page.scss'],
 })
 export class DetailPage implements OnInit {
-
-  constructor() { }
-
+ 
+  information = null;
+  
+  constructor(private activatedRoute: ActivatedRoute, private movieService: MovieService) { }
+ 
   ngOnInit() {
+    let id = this.activatedRoute.snapshot.paramMap.get('id');
+ 
+    this.movieService.getDetails(id).subscribe(result => {
+      this.information = result;
+    });
   }
-
+ 
+  openWebsite() {
+    window.open(this.information.Website, '_blank');
+  }
 }
