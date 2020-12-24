d6d836c0d9e709feea72bc14d9a97fe6913ea13e
pivotrooky <towagner@gmail.com>
2020-12-24 18:02:06 -0300

    Avoids unnecessary re-rending of list

--- client/src/app/movies/my-list/my-list.page.ts
+++ client/src/app/movies/my-list/my-list.page.ts
@@ -1,31 +1,45 @@
-import { MyListService } from '../../services/my-list.service';
-import { Component, Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { MyListService } from "../../services/my-list.service";
+import { Component, Injectable } from "@angular/core";
+import { Observable } from "rxjs";
 
 @Injectable({
   providedIn: "root",
 })
-
 @Component({
-  selector: 'app-search',
-  templateUrl: './my-list.page.html',
-  styleUrls: ['./my-list.page.scss'],
+  selector: "app-search",
+  templateUrl: "./my-list.page.html",
+  styleUrls: ["./my-list.page.scss"],
 })
 export class MyListPage {
-
   myListItems = [];
- 
-  constructor(private myListService: MyListService) { }
- 
+
+  constructor(private myListService: MyListService) {}
+
   ionViewDidEnter() {
     this.handleLocalSearch();
   }
- 
+
   handleLocalSearch() {
     this.myListService.getMyList().subscribe(
-      userData => {
-        this.myListItems = userData[0].movies;
-        /* console.log(this.myListItems); */
+      (userData) => {
+        const currentList = userData[0].movies;
+
+        if (currentList.length !== this.myListItems.length) {
+          //number of items changed, re-render list, no need to go inside loop
+          this.myListItems = currentList;
+          return;
+        }
+
+        for (let i = 0; i < currentList.length; i++) {
+          for (let prop in currentList[i]) {
+            if (currentList[i][prop] !== this.myListItems[i][prop]) {
+              //some data is different, so list should be re-rendered
+              this.myListItems = currentList;
+              return;
+            }
+          }
+        }
+        //avoid unnecessary re-rendering
       }
     );
   }
