6cc576aa7e3917a80249493b98efdca02e0cc91d
pivotrooky <towagner@gmail.com>
2020-12-24 15:11:29 -0300

    Improves routing. Automatically takes you to 'my-detail' when you go to the 'detail' component (which would fetch remote data), takes you to 'my-list' when you add a new item. Also fixes redundant load in 'my-list' when it is updated.

--- client/src/app/movies/my-list/my-detail/my-detail.page.ts
+++ client/src/app/movies/my-list/my-detail/my-detail.page.ts
@@ -1,38 +1,43 @@
-import { MyListService } from '../../../services/my-list.service';
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
- 
+import { MyListService } from "../../../services/my-list.service";
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { NavController } from "@ionic/angular";
+
 @Component({
-  selector: 'app-movie-details',
-  templateUrl: './my-detail.page.html',
-  styleUrls: ['./my-detail.page.scss'],
+  selector: "app-movie-details",
+  templateUrl: "./my-detail.page.html",
+  styleUrls: ["./my-detail.page.scss"],
 })
-export class MyDetailPage implements OnInit {
- 
+export class MyDetailPage{
   item = null;
   id = null;
-  
-  constructor(private router: Router, private activatedRoute: ActivatedRoute, private myListService: MyListService) { }
- 
-  ngOnInit() {
-    this.id = this.activatedRoute.snapshot.paramMap.get('id');
- 
-    this.myListService.getMyDetails(this.id).subscribe(result => {
+
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private myListService: MyListService,
+    public navCtrl: NavController
+  ) {}
+
+  ionViewDidEnter() {
+    //mejorar con caché pero tener cuidado de no renderizar información desactualizada?
+    this.id = this.activatedRoute.snapshot.paramMap.get("id");
+
+    this.myListService.getMyDetails(this.id).subscribe((result) => {
       this.item = result;
     });
   }
- 
+
   openWebsite() {
-    window.open(this.item.Website, '_blank');
+    window.open(this.item.Website, "_blank");
   }
 
   removeThisAndGetOut() {
     this.removeThisFromMyList();
-    this.router.navigate(['/myList']);
+    this.router.navigate(["/myList"]);
   }
 
   removeThisFromMyList() {
     return this.myListService.removeItem(this.id).subscribe();
   }
-  
 }
