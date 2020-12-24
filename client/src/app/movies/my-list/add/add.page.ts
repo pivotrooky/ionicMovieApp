4ede6c9a871a8294c90f1a1d00e9504d3f4c3ded
pivotrooky <towagner@gmail.com>
2020-12-24 19:28:55 -0300

    Adds basic visual structure of 'add' page

--- client/src/app/movies/my-list/add/add.page.ts
+++ client/src/app/movies/my-list/add/add.page.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { ActivatedRoute, Router } from "@angular/router";
 
 @Component({
-  selector: 'app-add',
-  templateUrl: './add.page.html',
-  styleUrls: ['./add.page.scss'],
+  selector: "app-add",
+  templateUrl: "./add.page.html",
+  styleUrls: ["./add.page.scss"],
 })
 export class AddPage implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  onCancel() {
+    this.router.navigate(["/myList"]);
   }
 
+  onAddItem() {
+    return;
+  }
 }
