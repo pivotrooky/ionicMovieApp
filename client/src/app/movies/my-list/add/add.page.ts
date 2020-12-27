0839dd4ca67cdd784e83941c524c5061919e6430
pivotrooky <towagner@gmail.com>
2020-12-26 22:24:28 -0300

    Now users can add a movie from scratch. Image URL validation still needs fix.

--- client/src/app/movies/my-list/add/add.page.ts
+++ client/src/app/movies/my-list/add/add.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild } from "@angular/core";
 import { NgForm } from "@angular/forms";
 import { ActivatedRoute, Router } from "@angular/router";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: "app-add",
@@ -8,7 +9,13 @@ import { ActivatedRoute, Router } from "@angular/router";
   styleUrls: ["./add.page.scss"],
 })
 export class AddPage implements OnInit {
-  constructor(private router: Router) {}
+  constructor(private router: Router, private http: HttpClient) {}
+
+  types = ["movie", "series"];
+  type = {};
+  //para que no dé error add.page.html
+
+  urlRegex = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/g;
 
   ngOnInit() {}
 
@@ -22,11 +29,43 @@ export class AddPage implements OnInit {
     this.movieForm.reset();
   }
 
-  onSubmit(form: NgForm) {
+  addMovieToList(form: NgForm) {
     console.log("Title is : " + form.value.title);
     console.log("Year is : " + form.value.year);
     console.log("Image is : " + form.value.image);
     console.log("Genre is : " + form.value.genre);
     console.log("Plot is : " + form.value.plot);
+    console.log("Type is:" + form.value.type);
+
+    const { title, genre, year, image, plot, type } = form.value;
+
+    if (!this.types.includes(type)) return console.log("error de type");
+    if (image !== "" && !this.urlRegex.test(image))
+      return console.log("error de image");
+
+    const userId = 1;
+    const newMovie = {
+      title,
+      genre,
+      year,
+      image,
+      plot,
+      type,
+      userId,
+    };
+    console.log(newMovie);
+    return this.http
+      .post("http://localhost:3001/movies/", newMovie)
+      .subscribe();
+  }
+
+  getBackToMyList() {
+    this.router.navigate(["/myList"]);
+    return;
+  }
+
+  onSubmit(f) {
+    this.addMovieToList(f);
+    this.getBackToMyList();
   }
 }
