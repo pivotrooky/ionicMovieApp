159e064b9f8804f6d6e92143e1140e4299de18ee
pivotrooky <towagner@gmail.com>
2020-12-30 15:58:07 -0300

    Breaks login/register pages in two (adds repeat-password input in register page).

---
+++ client/src/app/auth/register/register.page.ts
@@ -0,0 +1,50 @@
+import { NgForm} from "@angular/forms";
+import { Component, OnInit } from "@angular/core";
+import { AuthService } from "../../services/auth.service";
+import { Router } from "@angular/router";
+import { AlertController } from "@ionic/angular";
+
+@Component({
+  selector: "app-register",
+  templateUrl: "./register.page.html",
+  styleUrls: ["./register.page.scss"],
+})
+export class RegisterPage implements OnInit {
+
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private alertCtrl: AlertController,
+  ) {}
+
+  ngOnInit() {}
+
+  signup(email, password) {
+    this.authService
+      .signup(email, password)
+      .then((response) => {
+        console.log(response)
+      })
+      .catch(({error}) => this.showAlert(error));
+  }
+
+  onSubmit(form: NgForm) {
+    if (!form.valid) {
+      return;
+    }
+    const { email, password } = form.value;
+    form.reset();
+    this.signup(email, password);
+    return this.router.navigate(["/login"]);
+  }
+
+  showAlert(message: string) {
+    this.alertCtrl
+      .create({
+        header: "Authentication failed",
+        message,
+        buttons: ["Okay"],
+      })
+      .then((alertEl) => alertEl.present());
+  }
+}
